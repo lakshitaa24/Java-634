5c9872a4704770253805548f573e33a799a38f62
Lakshita Tangri <lakshitaa24@gmail.com>
2025-10-29 10:24:26 +0530

    Update employee records

---
+++ Experiment 2.4/Part A/FetchEmployeeData.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Salary DOUBLE
+);
+INSERT INTO Employee VALUES (101, 'Lakshita', 60000),
+                            (102, 'Nitin', 55000),
+                            (103, 'Asha', 50000);
